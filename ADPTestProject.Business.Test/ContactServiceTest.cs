73de8c09507b900f22134da3a378f9b417a7e756
SMTSOFTWARE\mona.shahinnejad <mona.shahinnejad@intive.com>
2018-08-14 10:22:16 +0200

    Add BussinessTest

---
+++ ADPTestProject.Business.Test/ContactServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ADPTestProject.Business.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ADPTestProject.Business.Test
+{
+    [TestClass]
+    public class ContactServiceTest
+    {
+        [TestMethod]
+        public void TestAddContactMethod()
+        {
+        }
+    }
+}
---
+++ ADPTestProject.Business.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ADPTestProject.Business.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ADPTestProject.Business.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d6a9a33d-cad7-4c15-a947-e03f387984f7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
