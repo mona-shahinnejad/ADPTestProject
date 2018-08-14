a7854d03761aafd1090ac17a448019362917587a
SMTSOFTWARE\mona.shahinnejad <mona.shahinnejad@intive.com>
2018-08-14 13:18:43 +0200

    add tests

--- ADPTestProject.Business.Test/ContactServiceTest.cs
+++ ADPTestProject.Business.Test/ContactServiceTest.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using ADPTestProject.Business.Dto;
 using ADPTestProject.Business.Service;
+using ADPTestProject.Data.Entities;
+using Autofac.Extras.Moq;
+using AutoMapper;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ADPTestProject.Business.Test
@@ -10,6 +14,30 @@ namespace ADPTestProject.Business.Test
         [TestMethod]
         public void TestAddContactMethod()
         {
+            using (var mock = AutoMock.GetLoose())
+            {
+                var contactDto = new ContactDto
+                {
+                    FirstName = "FirstName",
+                    LastName = "LastName",
+                    DateOfBirth = new DateTime(1990, 1, 1)
+                };
+
+                var contact = new Contact
+                {
+                    FirstName = "FirstName",
+                    LastName = "LastName",
+                    DateOfBirth = new DateTime(1990, 1, 1)
+                };
+
+                mock.Mock<IMapper>().Setup(m => m.Map<Contact>(contactDto)).Returns(contact);
+                mock.Mock<IMapper>().Setup(m => m.Map<ContactDto>(contact)).Returns(contactDto);
+                var contactService = mock.Create<ContactService>();
+
+                var addedContactDto = contactService.AddContact(contactDto);
+
+                Assert.IsNotNull(addedContactDto);
+            }
         }
     }
 }
---
+++ ADPTestProject.WebApi.Tests/Controllers/ContactApiServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+using ADPTestProject.Business.Dto;
+
+namespace ADPTestProject.WebApi.Tests.Controllers
+{
+    /// <summary>
+    /// Summary description for ContactApiServiceTests
+    /// </summary>
+    [TestClass]
+    public class ContactApiServiceTests
+    {
+        private const string serverAddress = "http://localhost:55050/";
+        private const string addContactAddress = "api/contact/add";
+
+        [TestMethod]
+        public void AddContactApiTest()
+        {
+            var contactDto = new ContactDto
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                DateOfBirth = new DateTime(1990, 1, 1)
+            };
+
+            var addUrl = serverAddress + addContactAddress;
+            var httpClient = new HttpClient();
+
+            var response = httpClient.PostAsJsonAsync(addUrl, contactDto);
+
+            response.Result.EnsureSuccessStatusCode();
+
+            string content = response.Result.Content.ReadAsStringAsync().Result;
+
+            Assert.IsTrue(!string.IsNullOrWhiteSpace(content) && content.Contains("FirstName"));
+        }
+    }
+}
